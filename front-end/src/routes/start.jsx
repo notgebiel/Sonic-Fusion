2a00cf6a554fc429f9be55f79bcc53e486f66b9a
notgebiel <cloostermansemile@gmail.com>
2024-03-17 21:00:52 +0100

    added top structure in start.jsx

--- front-end/src/routes/start.jsx
+++ front-end/src/routes/start.jsx
@@ -1,12 +1,23 @@
 import NavBar from "../components/navbar";
 import React from "react";
 import '../stylesheets/general.css';
+import styles from '../stylesheets/start.module.css';
+import Image from "../fotos_front_page/thumbnail_6DDD42625B4144F58408D4CD84DAB2BE.png"
 
 export default function Start() {
     return(
         <>
             <NavBar />
-            
+            <div className={styles.starttopdiv}>
+                <div className={styles.starttoptekstdiv}>
+                    <h1>Wie is Sonic Fusion</h1>
+                    <h4>placeholder tekst</h4>
+                </div>
+                <div className={styles.starttopfotodiv}>
+                    <img className={styles.foto} src={Image} alt=""></img>
+                </div>
+                
+            </div>
         </>
     )
 }
