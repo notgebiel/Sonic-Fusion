b3bc5845e73e6202e5f98b1dfd1cd18593fc4623
notgebiel <cloostermansemile@gmail.com>
2024-03-18 09:06:48 +0100

    voorlopige merch page afgewerkt

--- front-end/src/routes/merch.jsx
+++ front-end/src/routes/merch.jsx
@@ -1,10 +1,11 @@
 import NavBar from "../components/navbar"
+import styles from '../stylesheets/merch.module.css';
 
 export default function MerchPage() {
     return (
         <>
             <NavBar active="Merch" />
-            <h1>Merch page</h1>
+            <h1 className={styles.comingsoon}>Coming Soon</h1>
         </>
     )
 }
