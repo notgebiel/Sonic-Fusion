5bcf4dcdc002f15b89065bb4c43c9f1938df494f
notgebiel <cloostermansemile@gmail.com>
2024-04-17 09:43:14 +0200

    added functional social media icons

--- front-end/src/components/navbar.js
+++ front-end/src/components/navbar.js
@@ -1,7 +1,10 @@
 import '../stylesheets/navbar.css'
 import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IoLogoInstagram } from "react-icons/io5";
+import { SiTiktok } from "react-icons/si";
+import { PiSoundcloudLogoFill } from "react-icons/pi";
+
+
 
 
 
@@ -20,7 +23,9 @@ export default function NavBar(props) {
                 <li id="navli" className={active === 'Prijzen' ? 'active' : ''} onClick={() => handleItemClick('Prijzen')}><a href="/prijzen">Prijzen</a></li>
                 <li id="navli" className={active === 'Boeken' ? 'active' : ''} onClick={() => handleItemClick('Boeken')}><a  href="/boeken">Boeken</a></li>
                 <li id="navli" className={active === 'Merch' ? 'active' : ''} onClick={()=> handleItemClick('Merch')}><a href="/merch">Merch</a></li>
-                <li id="navli" className='smicons'><a href=""><IoLogoInstagram /></a></li>
+                <li id="navliicon" className='smicons'><a href="https://www.instagram.com/dj_sonic_fusion/" target='_blank'><IoLogoInstagram /></a></li>
+                <li id="navliicon" className='smicons'><a href="https://www.tiktok.com/@dj_sonic_fusi0n" target='_blank'><SiTiktok /></a></li>
+                <li id="navliicon" className='smicons'><a href="https://on.soundcloud.com/wySEbzEArqvmLTNz5" target="_blank"><PiSoundcloudLogoFill /></a></li>
             </ul>
         </div>
     )
