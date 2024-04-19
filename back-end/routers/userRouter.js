3b7df17d7c5c476d52a5c78545eeaa04c494c799
notgebiel <cloostermansemile@gmail.com>
2024-04-19 11:54:18 +0200

    userRouter finished

--- back-end/routers/userRouter.js
+++ back-end/routers/userRouter.js
@@ -24,3 +24,27 @@ router.post('users/login', async (req, res) => {
         res.status(400).send(error);
     }
 })
+
+router.post('users/logout', Auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token;
+        });
+        await req.users.save();
+        res.send();
+    }catch (error) {
+        res.status(500).send();
+    }
+});
+
+router.post('users/logoutAll', Auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.send();
+    }catch(error) {
+        res.status(500).send();
+    }
+});
+
+module.exports = router;
