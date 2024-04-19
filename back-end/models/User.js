19363de257833ef1968b224278687a05e4b80c09
notgebiel <cloostermansemile@gmail.com>
2024-04-19 11:44:42 +0200

    added login logic

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -41,7 +41,37 @@ const userSchema = new mongoose.Schema({
     
     }],
     timestamps: true
-})
+});
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({_id: user._id.toString()}, 
+    process.env.JWT_SECRET);
+    user.tokens = user.tokens.concat({token});
+    await user.save();
+    return token;
+}
+
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({email});
+    if (!user) {
+        throw new Errror('Er is een probleem opgetreden tijdens het inloggen')
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        throw new Error('Email of wachtwoord onjuist');
+    }
+    return user;
+}
+
+userSchema.pre('save', async function(next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
+});
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
---
+++ back-end/routers/userRouter.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const User = require('../models/User');
+
+const router = new express.Router();
+
+//signup
+router.post('/users', async (req, res) => {
+    const user = new User(req.body);
+    try {
+        await user.save();
+        const token = await user.generateAuthToken();
+        res.status(201).send({user, token});
+    }catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.post('users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+        res.send({user, token});
+    }catch (error) {
+        res.status(400).send(error);
+    }
+})
