8539c1119a9d7255a6e647011604b956b152186e
notgebiel <cloostermansemile@gmail.com>
2024-04-22 09:59:33 +0200

    finished itemRouter

--- back-end/routers/itemRouter.js
+++ back-end/routers/itemRouter.js
@@ -39,4 +39,44 @@ router.get('/items', async(req, res) => {
     }catch(error) {
         res.status(400).send(error);
     }
-})
+});
+
+
+//update
+router.patch('/items/:id', Auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'description', 'category', 'price'];
+    
+    const isValidOperation = updates.every(async (update) => allowedUpdates.includes(updates))
+
+        if(!isValidOperation) {
+            return res.status(400).send({error: 'invalid updates'})
+        }
+         try {
+            const item = await Item.findOne({_id: req.params.id});
+            if (!item) {
+                return res.status(404).send()
+            }
+            updates.forEach((update) => item[update] = req.body[update]);
+            await item.save();
+            res.send(item);
+        }catch (error) {
+            res.status(400).send(error);
+        }
+    
+});
+
+//delete
+router.delete('/items/:id', Auth, async(req, res) => {
+    try {
+        const deletedItem = await Item.findOneAndDelete({_id: req.params.id});
+        if(!deletedItem) {
+            res.status(404).send({error: 'Item not found'});
+        }
+        res.send(deletedItem);
+    }catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+module.exports = router;
