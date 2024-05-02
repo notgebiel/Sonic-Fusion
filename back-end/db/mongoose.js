25cb0e4279544591fcdc3604802e603696baee86
notgebiel <cloostermansemile@gmail.com>
2024-05-02 11:07:38 +0200

    removed broken API

--- back-end/db/mongoose.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config()
-
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-});
--- back-end/server.js
+++ back-end/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 10000;
 const cors = require('cors');
-require('./db/mongoose');
 
 
 
