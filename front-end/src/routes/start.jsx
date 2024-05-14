ee71ced7d69b380b71c4dde9f7b638faa8687315
notgebiel <cloostermansemile@gmail.com>
2024-05-14 10:00:09 +0200

    added smoothscroll to boeken button

--- front-end/src/routes/start.jsx
+++ front-end/src/routes/start.jsx
@@ -1,5 +1,5 @@
 import NavBar from "../components/navbar";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import '../stylesheets/general.css';
 import styles from '../stylesheets/start.module.css';
 import Image from "../fotos_front_page/thumbnail_6DDD42625B4144F58408D4CD84DAB2BE.png"
@@ -30,6 +30,15 @@ const Start = () => {
 }
 
 const DesktopStart = () => {
+    
+    const boekenRef = useRef(null);
+
+    const scrollToDiv = () => {
+        if (boekenRef.current) {
+            boekenRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
     return (
         <>
             <NavBar />
@@ -40,9 +49,7 @@ const DesktopStart = () => {
                         We hebben al op verschillende evenementen gedraaid.<br />
                         We zorgen voor een goeie sfeer op het feest dat u nooit zult vergeten.
                     </h3>
-                    <Link to="/boeken">
-                        <button className={styles.button}><p className={styles.buttonp}>Boek nu</p></button>
-                    </Link>
+                        <button className={styles.button} onClick={scrollToDiv}><p className={styles.buttonp}>Boek nu</p></button>
                 </div>
                 <div className={styles.starttopfotodiv}>
                     <img className={styles.foto} src={Image} alt="DJ Sonic Fusion"></img>
@@ -52,9 +59,13 @@ const DesktopStart = () => {
                 <span style={{ textAlign: "center", marginTop: '0' }}><h2>Prijzen</h2></span>
                 <h3 className={styles.prijzentekst}>prijzentekst</h3>
             </div>
-            <div className={styles.boekendiv}>
-                <span style={{textAlign: "center", marginTop: '0'}}><h2>Boeken</h2></span>
-                <h3 className={styles.boekentekst}>Boekentekst</h3>
+            <div className={styles.boekendiv} ref={boekenRef}>
+                <span style={{textAlign: "center", marginTop: '0', marginBottom: '0'}}><h2>Boeken</h2></span>
+                <h3 className={styles.boekentekst}>
+                    Geïnteresseerd?<br/>
+                    Boeken kan via onze <a href="https://www.instagram.com/dj_sonic_fusion/" target="_blank">Instagram pagina.</a><br/>
+                    Stuur ons een DM en wij antwoorden hier zo snel mogelijk op. Tot dan!
+                </h3>
             </div>
 
 
@@ -63,6 +74,15 @@ const DesktopStart = () => {
 }
 
 const MobileStart = () => {
+
+    const boekenRef = useRef(null);
+
+    const scrollToDiv = () => {
+        if (boekenRef.current) {
+            boekenRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
     return (
         <>
             <NavBar />
@@ -73,14 +93,25 @@ const MobileStart = () => {
                         We hebben al op verschillende evenementen gedraaid.<br />
                         We zorgen voor een goeie sfeer op het feest dat u nooit zult vergeten.
                     </h3>
-                    <Link to="/boeken">
-                        <button className={styles.button}><p className={styles.buttonp}>Boek nu</p></button>
-                    </Link>
+                        <button className={styles.button} onClick={scrollToDiv}><p className={styles.buttonp}>Boek nu</p></button>
                 </div>
                 <div className={styles.starttopfotodivmb}>
                     <img className={styles.foto} src={Image} alt="DJ Sonic Fusion"></img>
                 </div>
             </div>
+            <div className={styles.prijzendiv}>
+                <span style={{ textAlign: "center", marginTop: '0' }}><h2>Prijzen</h2></span>
+                <h3 className={styles.prijzentekst}>prijzentekst</h3>
+            </div>
+            <div className={styles.boekendiv} ref={boekenRef}>
+                <span style={{textAlign: "center", marginTop: '0', marginBottom: '0'}}><h2>Boeken</h2></span>
+                <h3 className={styles.boekentekst}>
+                    Geïnteresseerd?<br/>
+                    Boeken kan via onze <a href="https://www.instagram.com/dj_sonic_fusion/" target="_blank">Instagram pagina.</a><br/>
+                    Stuur ons een DM en wij antwoorden hier zo snel mogelijk op. Tot dan!
+                </h3>
+            </div>
+
         </>
     )
 }
