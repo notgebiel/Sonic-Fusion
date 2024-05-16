811367e1937e5b6976c5b6da651c4607e7272108
notgebiel <cloostermansemile@gmail.com>
2024-05-16 11:19:33 +0200

    opnieuw begonnen aan server

--- back-end/server.js
+++ back-end/server.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 10000;
 const cors = require('cors');
+const axios = require('axios');
 
-
-
-
+let mainToSfMain = null;
 app.use(cors());
 app.use(express.json());
 
@@ -20,7 +19,20 @@ app.post('/', (req, res) => {
     console.log('data: ', data);
 
     res.json({message: 'data received'});
+    mainToSfMain = data;
 })
 
+//send data to parent server
+app.get('/sf/main', async (req, res) => {
+    try {
+        const response = await axios.post('http://localhost:3001/sf/main', {
+            key: 'sleutel',
+            data: mainToSfMain
+        });
+        res.send(`data sent to parent server ${response.data}`)
+    }catch (error) {
+        res.status(500).send(`Error sending data to parent server: ${error.message}`);
+    }
+})
 
 app.listen(port, console.log(`server listening on port ${port}`));
